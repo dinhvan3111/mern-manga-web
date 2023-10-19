d8a8495a1a5aec416296ff394b7bc7cf5d3b7cff
dinhvan3111 <85469453+dinhvan3111@users.noreply.github.com>
2023-10-19 22:50:36 +0700

    change endPoint

--- client/src/configuration/config.js
+++ client/src/configuration/config.js
@@ -3,10 +3,16 @@ import environmentConfig from "./enviromentConfig";
 
 const isDevEnv =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+console.log("!process.env.NODE_ENV", !process.env.NODE_ENV);
+console.log(
+  ` process.env.NODE_ENV === "development"`,
+  process.env.NODE_ENV === "development"
+);
+console.log("isDevEnv", isDevEnv);
 const envConfig = isDevEnv
   ? environmentConfig.development
   : environmentConfig.production;
-
+console.log("envConfig", envConfig);
 const Config = {
   isDevEnv,
   apiConfig: {
